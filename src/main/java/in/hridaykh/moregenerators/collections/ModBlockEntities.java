27052039bbd0dcfddf8bd21ecdbbc56506f099c2
HridayKh <hridaykh1234@gmail.com>
2026-06-08 16:21:11 +0530

    built

--- src/main/java/in/hridaykh/moregenerators/collections/ModBlockEntities.java
+++ src/main/java/in/hridaykh/moregenerators/collections/ModBlockEntities.java
@@ -12,8 +12,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.Supplier;
 
 public class ModBlockEntities {
-	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE,
-			MoreGenerators.MOD_ID);
+	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, MoreGenerators.MOD_ID);
 	@SuppressWarnings("null")
 	public static final Supplier<BlockEntityType<ResistorBE>> RESISTOR_BE = BLOCK_ENTITIES.register("resistor",
 			() -> BlockEntityType.Builder.of(ResistorBE::new, ModBlocks.LIGHT_RESISTOR.get()).build(null));
